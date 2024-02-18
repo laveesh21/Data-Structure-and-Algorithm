a2689a695792f71b8e9ba199f030764563797f81
laveesh21 <laveeshtomar.royalking@gmail.com>
2024-02-18 22:33:48 +0530

    Added: Fibonacci via Recursion

--- src/Recursion/03_Fibonacci.cpp
+++ src/Recursion/03_Fibonacci.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 
+int f(int n){
+    if(n<=1){
+        return n;
+    }
+    int last = f(n-1);
+    int slast = f(n-2);
+
+    return last + slast;
+}
+
 int main(){
 
-    
+    std::cout << f(7);
 
     return 0;
 }
