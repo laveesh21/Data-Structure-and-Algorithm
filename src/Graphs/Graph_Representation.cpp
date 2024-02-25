90c012c60dd9cd6414cdff4c0ad5502878f41db4
laveesh21 <laveeshtomar.royalking@gmail.com>
2024-02-25 19:10:50 +0530

    Added Graph Representation file

---
+++ src/Graphs/Graph_Representation.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+
+
+int main(){
+
+    int n,m;
+    std::cin >> n >> m;
+
+    std::vector<std::vector<int>> adj(n+1);
+
+    for(int i=1; i<m; i++){
+        int u,v;
+        std::cin >> u >> v;
+        adj[u].push_back(v); 
+        adj[v].push_back(u);
+    }
+
+    return 0;
+}
